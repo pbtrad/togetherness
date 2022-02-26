40bd96855a5eddac96d83087e9daa3add2f133e8
AnaG0307 <ana.genover@gmail.com>
2022-02-26 17:42:31 +0000

    UPdated allauth to last version, migrate changes and added details to settings .py

--- togetherness/urls.py
+++ togetherness/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user_profile.urls')),
-    path('accounts', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
 ]
 
