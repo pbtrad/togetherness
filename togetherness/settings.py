37e41eb4d58bf3280419d3f4bd620b942412200d
paul <paulbrowne2@gmail.com>
2022-02-26 05:41:10 +0000

    Created chat, user profile and checkout apps, along with initial templates

--- togetherness/settings.py
+++ togetherness/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user_profile',
+    'chat',
+    'checkout',
 ]
 
 MIDDLEWARE = [
